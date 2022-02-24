d30321287103e0cc2d4bbb3e597b9619b1850857
Yoltic2 <eyolticmg@icloud.com>
2022-02-24 09:13:15 -0600

    Segunad version previa revision del prof

--- resultado.py
+++ resultado.py
@@ -1,14 +1,16 @@
-
-from itertools import count
+# Emilio Yoltic Martinez Gutierrez 
 import re
+import os
 
+# Desplegamos las opciones del menu
 def menu():
     print("""
         1.- Multiply sets
         2.- Power Sets
-        3.- Exit
+        3.- Determine Relation
+        4.- Exit
     """)
-    option = int(input("Insert the option "))
+    option = str(input("Insert the option "))
     return option
 
 
@@ -52,17 +54,107 @@ def powerSet():
 # Por regla general los sets llevan un set vacio
     print(newSet)
 
+def getNewSet(set):
+    newSet=[]
+    A=[]
+    B=[]
+    for i in range(len(set)):
+        if(i%6== 1 ):
+            newSet.append((set[i],set[i+2]))
+            A.append(set[i])
+            B.append(set[i+2])
+    return newSet,A,B
+
+def check_if_it_is_transitive(set):
+    newSet,A,B = getNewSet(set)
+    print(A,"   ",B)
+    # Todo es falso hasta que se demuestre lo contrario
+    res=False
+    print(newSet)
+    if newSet == [(1,1)]:
+        return True
+    # formula (a,b) and (a,c) 
+    for i in range(0,len(A),2):
+        if ((A[-2],B[-1]) in newSet):
+            res=True
+        elif ((A[i%len(A)],B[(i+1)%len(B)])in newSet):
+            if ((A[i%len(A)],B[(i+2)%len(B)])in newSet):
+                res = True
+        else:
+            return False
+    print(newSet)
+    return res
+
+
+def check_if_it_is_reflexiv(set):
+    newSet,A,B = getNewSet(set)
+    res= False
+    for i in range(len(A)):
+        if(A[i]==B[i]):
+            res=True
+        else:
+            return False
+    return res
+
+def simplyfy(A,B): 
+    Asimple=[0 for  x in range(len(A))]
+    Bsimple=[0 for  x in range(len(B))]
+    for i in range(len(A)):
+        if(A[i]not in Asimple):
+            Asimple[i]=A[i]
+    for i in range(len(B)):
+        if(B[i]not in Bsimple):
+            Bsimple[i]=B[i]
+    for i in range(len(Asimple)):
+        if 0 in Asimple:
+            Asimple.remove(0)
+        else:
+            break
+    for i in range(len(Bsimple)):
+        if 0 in Bsimple:
+            Bsimple.remove(0)
+        else:
+            break
+    return Asimple,Bsimple
+            
+
+
+def check_if_it_is_symetric(set):
+    newSet,A,B = getNewSet(set)
+    res= False
+    Asimple,Bsimple=simplyfy(A,B)
+    print(Asimple,"   ",Bsimple)
+    # if ((Asimple[1%len(Asimple)],Bsimple[2%len(Bsimple)])in newSet and (Asimple[1],Bsimple[2]) in newSet):
+    #     return True
+
+    return res
+
+
+def determinate():
+    set= str(input("Write set:"))
+    transitive=check_if_it_is_transitive(set)
+    reflexiv=check_if_it_is_reflexiv(set)
+    symetric=check_if_it_is_symetric(set)
+    print("transitive:",transitive,"Reflexiv:",reflexiv,"Symetric:",symetric)
+
 
 def main():
+    state= "Everthing good"
     while True:
+        print(state)
         opcion = menu()
-        if(opcion ==1):
+        if(opcion =="1"):
             multiply()
-        if (opcion == 2 ):
+        if (opcion == "2"):
             powerSet()
-        if(opcion == 3):
-            return 
-            
+        if(opcion == "3"):
+            determinate() 
+        if (opcion =="4"):
+            return
+        else:
+            state="Not valid Option"
+            print("Opcion no valida ")
+        os.system("clear")
 
 if __name__ == '__main__':
     main()
